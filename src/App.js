ddc0a4f26a400c79b7c81fff7883d7d035d7435e
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-16 03:58:31 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import Header from './components/Header.js';
+import WelcomeWorld from './components/WelcomeWorld.js';
+import CreateGame from './components/CreateGame.js';
+import Register from './components/Register.js';
+import Login from './components/Login.js';
+import EditGame from './components/EditGame.js';
+import GameDetails from './components/GameDetails.js';
+
+
 
 
 function App() {
   return (
-    <div>
-    
+    <div id="box">
+      <Header />
+      <main id="main-content">
+      <WelcomeWorld />
+      </main>  
     </div>
   );
-}
+};
 
 export default App;
