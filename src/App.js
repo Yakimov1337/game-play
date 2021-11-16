55ebae20bbf16442e07570a2a53b7a78334b4395
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-16 04:04:25 +0200

    Add Routes

--- src/App.js
+++ src/App.js
@@ -5,17 +5,27 @@ import Register from './components/Register.js';
 import Login from './components/Login.js';
 import EditGame from './components/EditGame.js';
 import GameDetails from './components/GameDetails.js';
+import Catalog from './components/Catalog.js';
 
 
 
 
 function App() {
+  const routers = {
+    '/home': WelcomeWorld,
+    '/games': Catalog,
+    '/register': Register,
+    '/edit': EditGame,
+    '/details': GameDetails,
+    '/login': Login,
+    '/create': CreateGame
+  }
   return (
     <div id="box">
       <Header />
       <main id="main-content">
-      <WelcomeWorld />
-      </main>  
+        <WelcomeWorld />
+      </main>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
 export default function Header() {
+    const onHeaderClick = (e) => {
+        e.preventDefault();
+
+        if (e.target.tagName == 'A') {
+            let url = new URL(e.target.href);
+        }
+    }
     return (
-        <header>
+        <header onClick={onHeaderClick}>
             <h1><a className="home" href="/home">GamesPlay</a></h1>
             <nav>
                 <a href="/games">All games</a>
