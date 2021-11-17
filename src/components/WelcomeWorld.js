c6bd09e4ac9e83d50f1e33bd4bfa745479385675
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-17 03:20:19 +0200

    Latest games

--- src/components/WelcomeWorld.js
+++ src/components/WelcomeWorld.js
@@ -1,57 +1,37 @@
-export default function WelcomeWorld() {
-    return (
-        <section id="welcome-world">
+import * as gameService from '../services/gameService.js';
+import { useState, useEffect } from 'react';
+import LatestGameCard from './LatestGameCard.js';
 
-        <div className="welcome-message">
-          <h2>ALL new games are</h2>
-          <h3>Only in GamesPlay</h3>
-        </div>
-        <img src="./images/four_slider_img01.png" alt="hero" />
+export default function WelcomeWorld({
+  navigationChangeHandler
+}) {
+  const [games, setGames] = useState([]);
 
-        <div id="home-page">
-          <h1>Latest Games</h1>
+  useEffect(async () => {
+    let result = await gameService.getLatest();
+    setGames(result);
+  });
+  return (
+    <section id="welcome-world">
 
+      <div className="welcome-message">
+        <h2>ALL new games are</h2>
+        <h3>Only in GamesPlay</h3>
+      </div>
+      <img src="./images/four_slider_img01.png" alt="hero" />
 
-          <div className="game">
-            <div className="image-wrap">
-              <img src="./images/CoverFire.png" />
-            </div>
-            <h3>Cover Fire</h3>
-            <div className="rating">
-              <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-            </div>
-            <div className="data-buttons">
-              <a href="#" className="btn details-btn">Details</a>
-            </div>
-          </div>
-          <div className="game">
-            <div className="image-wrap">
-              <img src="./images/ZombieLang.png" />
-            </div>
-            <h3>Zombie Lang</h3>
-            <div className="rating">
-              <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-            </div>
-            <div className="data-buttons">
-              <a href="#" className="btn details-btn">Details</a>
-            </div>
-          </div>
-          <div className="game">
-            <div className="image-wrap">
-              <img src="./images/MineCraft.png" />
-            </div>
-            <h3>MineCraft</h3>
-            <div className="rating">
-              <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-            </div>
-            <div className="data-buttons">
-              <a href="#" className="btn details-btn">Details</a>
-            </div>
-          </div>
+      <div id="home-page">
+        <h1>Latest Games</h1>
 
-
-          <p className="no-articles">No games yet</p>
-        </div>
-      </section>
-    );
+          {
+            games.length>0
+            ? games.map(x=> <LatestGameCard 
+              key={x._id} 
+              navigationChangeHandler={navigationChangeHandler} 
+              game={x} />)
+            : <p className="no-articles">No games yet</p>
+          }
+      </div>
+    </section>
+  );
 };
