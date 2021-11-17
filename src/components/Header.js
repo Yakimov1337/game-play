43b656ad12e3c599451b68aa76a38e6f05af8ce0
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-17 02:17:26 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,12 +14,12 @@ export default function Header({
             <h1><a className="home" href="/home">GamesPlay</a></h1>
             <nav>
                 <a href="/games">All games</a>
-                Logged-in users
+             
                 <div id="user">
                     <a href="/create-game">Create Game</a>
                     <a href="/logout">Logout</a>
                 </div>
-                Guest users
+            
                 <div id="guest">
                     <a href="/login">Login</a>
                     <a href="/register">Register</a>
