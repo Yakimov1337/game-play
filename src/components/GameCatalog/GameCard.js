1af2db6533dc242915dbd0901115e17e9dfbed6d
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-12-01 15:27:06 +0200

    Rating

--- src/components/GameCatalog/GameCard.js
+++ src/components/GameCatalog/GameCard.js
@@ -1,20 +1,19 @@
 import { Link } from "react-router-dom";
 const GameCard = ({
-    game,
+  game,
 
-})=> {
-  
-    return (
-        <div className="allGames">
-        <div className="allGames-info">
-          <img src={game.imageUrl} />
-          <h6>{game.category}</h6>
-          <h2>{game.title}</h2>
-          <Link to={`/games/${game._id}`} className="details-button">Details</Link>
-        </div>
+}) => {
 
+  return (
+    <div className="allGames">
+      <div className="allGames-info">
+        <img src={game.imageUrl} />
+        <h6>{game.category}</h6>
+        <h2>{game.title}</h2>
+        <Link to={`/games/${game._id}`} className="details-button">Details</Link>
       </div>
-    )
+    </div>
+  )
 };
 
 export default GameCard;
---
+++ src/utils/rating.js
@@ -0,0 +1,3 @@
+export default function(name) {
+    return Math.random()*10;
+}
